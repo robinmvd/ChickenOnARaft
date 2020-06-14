d12d1c782226cd23492f40f9557dca49ddaf3b9d
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-06-14 20:53:53 +0200

    Maak vlotten aan in game & gameloop.

--- dev/game.ts
+++ dev/game.ts
@@ -2,12 +2,20 @@
 
 class Game {
     
+    //Fields
     private trees:Tree[] = []
+    private bullets:Bullet[] = []
+
+    public addBullet(bullet : Bullet) {
+        this.bullets.push(bullet)
+    }
  
     constructor() {
         // de game heeft trees nodig
-
-
+        for (let i = 0; i < 4; i++) {
+            this.trees.push(new Tree(-100, i * 250 + 100, this))
+            
+        }
         
         // start de game loop
         this.gameLoop()
@@ -15,9 +23,13 @@ class Game {
     
     private gameLoop(){
         // gebruik een for..of loop om de move functie van alle trees in de array aan te roepen
-
-        
-                
+        for (const tree of this.trees) {
+            tree.move()
+        }
+        for (const bullet of this.bullets) {
+            bullet.move()
+        }
+      
         // animation
         requestAnimationFrame(() => this.gameLoop())
     }
