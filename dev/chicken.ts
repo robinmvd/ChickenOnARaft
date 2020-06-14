7ddc8e5a215437548ebabeabc74e236a7594ba55
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-06-14 20:54:13 +0200

    Voeg gun toe aan kippen.

--- dev/chicken.ts
+++ dev/chicken.ts
@@ -2,26 +2,42 @@
 
 class Chicken {
     
-    public div: HTMLElement
+    //Fields
+    private _div: HTMLElement    
     private x:number
     private y:number
-    private width:number
-    private height:number
 
-    constructor(x:number, y:number, tree:Tree) {
-        this.div = document.createElement("bird")
-        tree.div.appendChild(this.div)
+    private gun : Gun
+    private game : Game
+
+    //Properties
+    public get div(): HTMLElement {return this._div}
+
+    constructor(x:number, y:number, tree:Tree, game : Game) {
+        this._div = document.createElement("bird")
+        tree.div.appendChild(this._div)
+
+        this.game = game
                 
         this.x = x
         this.y = y
-        this.width = 67
-        this.height = 110
         
-        this.div.style.transform = `translate(${this.x}px, ${this.y}px)`
+        this._div.style.transform = `translate(${this.x}px, ${this.y}px)`
 
+        //this.gun = new Gun(this)
         // maak hier een click listener
         // de click event handler moet een gun toevoegen
         // als de kip al een gun heeft, dan moet de fire() functie van de gun worden aangeroepen
+        this._div.addEventListener("click", () => this.handleClick())
     }
 
+    handleClick(): void {
+        console.log("Chiken click! ")
+
+        if(!this.gun) {
+            this.gun = new Gun(this, this.game)
+        }
+        this.gun.fire()
+    }
+ 
 }
